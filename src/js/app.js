66265b3d3b03f58618375de2b6503822b775d484
Aleksander <Zakharov.owl@gmail.com>
2020-09-30 22:49:51 +0300

    fix: bag with replace 14str

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+let galleryImages = document.querySelectorAll('.gallery-img');
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages){
+    galleryImages.forEach(function(i, index){
+        i.onclick = function(){
+            let getElementCss = window.getComputedStyle(i);
+            /*console.log(getElementCss);*/
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            /*console.log(getFullImgUrl);*/
+            let getImgUrlPos = getFullImgUrl.split("/preview/");
+            /*console.log(getImgUrlPos);*/
+            let setNewImgUrl = getImgUrlPos[1].replace(`")`, ``);
+            /*console.log(setNewImgUrl);*/
+            getLatestOpenedImg = index + 1;
+/*
+            let container = document.body;
+            let newImgWindow = document.createElement('div');
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute('class', 'img-window')
+            newImgWindow.setAttribute('onclick','closeImg()')
+
+            let newImgWindow = document.createElement('img')
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute('scr','img/' + setNewImgUrl)*/
+        }
+    });
+}

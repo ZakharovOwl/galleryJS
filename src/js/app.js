a66e19f0b071c16516613e7207a596b6e3811945
Aleksander <Zakharov.owl@gmail.com>
2020-10-01 22:35:19 +0300

    feat: add closeImg fnc

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,34 @@ if(galleryImages){
             let newImg = document.createElement('img')
             newImgWindow.appendChild(newImg);
             newImg.setAttribute('src',`/assets/img/${setNewImgUrl}`)
-            
+
+            newImg.onload = function(){
+            let imgWidth = this.width;
+            let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80 ;
+
+            //btn next
+            let newNextBtn = document.createElement('a')
+            let btnNextText = document.createTextNode('Next')
+            newNextBtn.appendChild(btnNextText)
+            container.appendChild(newNextBtn)
+            newNextBtn.setAttribute('class', 'img-btn-next')
+            newNextBtn.setAttribute('onclick', 'changeImg()')
+            newNextBtn.style.cssText = `right:${calcImgToEdge}px`
+            //btn back
+            let newPrevBtn = document.createElement('a')
+            let btnPrevText = document.createTextNode('Back')
+            newPrevBtn.appendChild(btnPrevText)
+            container.appendChild(newPrevBtn)
+            newPrevBtn.setAttribute('class', 'img-btn-prev')
+            newPrevBtn.setAttribute('onclick', 'changeImg()')
+            newPrevBtn.style.cssText = `left:${calcImgToEdge}px`
+            }
         }
     });
+}
+
+function closeImg(){
+    document.querySelector('.img-window').remove()
+    document.querySelector('.img-btn-next').remove()
+    document.querySelector('.img-btn-prev').remove()
 }

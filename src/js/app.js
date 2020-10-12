0fa7a2d90dca97f3499505869e49d2344435cbb6
Aleksander <Zakharov.owl@gmail.com>
2020-10-12 20:39:24 +0300

    feat: add full app

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ if(galleryImages){
             let getImgUrlPos = getFullImgUrl.split("/preview/");
             /*console.log(getImgUrlPos);*/
             let setNewImgUrl = getImgUrlPos[1].replace(`")`, ``);
-            console.log(setNewImgUrl);
+            /*console.log(setNewImgUrl);*/
             getLatestOpenedImg = index + 1;
 
             let container = document.body;
@@ -23,11 +23,12 @@ if(galleryImages){
 
             let newImg = document.createElement('img')
             newImgWindow.appendChild(newImg);
-            newImg.setAttribute('src',`/assets/img/${setNewImgUrl}`)
+            newImg.setAttribute('src',`/assets/img/${setNewImgUrl}`);
+            newImg.setAttribute('id',`current-img`)
 
             newImg.onload = function(){
             let imgWidth = this.width;
-            let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80 ;
+            let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 79;
 
             //btn next
             let newNextBtn = document.createElement('a')
@@ -35,7 +36,7 @@ if(galleryImages){
             newNextBtn.appendChild(btnNextText)
             container.appendChild(newNextBtn)
             newNextBtn.setAttribute('class', 'img-btn-next')
-            newNextBtn.setAttribute('onclick', 'changeImg()')
+            newNextBtn.setAttribute('onclick', 'changeImg(1)')
             newNextBtn.style.cssText = `right:${calcImgToEdge}px`
             //btn back
             let newPrevBtn = document.createElement('a')
@@ -43,15 +44,53 @@ if(galleryImages){
             newPrevBtn.appendChild(btnPrevText)
             container.appendChild(newPrevBtn)
             newPrevBtn.setAttribute('class', 'img-btn-prev')
-            newPrevBtn.setAttribute('onclick', 'changeImg()')
+            newPrevBtn.setAttribute('onclick', 'changeImg(0)')
             newPrevBtn.style.cssText = `left:${calcImgToEdge}px`
             }
         }
     });
 }
-/*check*/
+
 function closeImg(){
     document.querySelector('.img-window').remove()
     document.querySelector('.img-btn-next').remove()
     document.querySelector('.img-btn-prev').remove()
+}
+
+function changeImg(changeDir){
+    document.querySelector('#current-img').remove()
+
+    let getImgWindow = document.querySelector('.img-window');
+    let newImg = document.createElement('img');
+    getImgWindow.appendChild(newImg)
+
+    let calcNewImg;
+    if(changeDir === 1){
+        calcNewImg = getLatestOpenedImg +1;
+        if(calcNewImg>galleryImages.length){
+            calcNewImg = 1;
+        }
+    }
+    else if(changeDir === 0){
+        calcNewImg = getLatestOpenedImg -1;
+        if(calcNewImg <1){
+            calcNewImg = galleryImages.length
+        }
+    }
+
+    newImg.setAttribute('src', 'assets/img/img' + calcNewImg + '.jpg')
+    newImg.setAttribute('id', 'current-img')
+
+    getLatestOpenedImg = calcNewImg;
+
+    newImg.onload - function(){
+        let imgWidth =this.width;
+        let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+        let nextBtn = document.querySelector('.img-btn-next')
+        nextBtn.style.cssText = `right:${calcImgToEdge}px`;
+
+        let prevBtn = document.querySelector('.img-btn-next')
+        prevBtn.style.cssText = `right:${calcImgToEdge}px`;
+    }
 }
